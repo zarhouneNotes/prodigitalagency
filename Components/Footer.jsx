b17f3e87a40e78c59bade4e738eb9445179bed07
EL HOUSSINE ZARHOUNE <94381264+zarhouneNotes@users.noreply.github.com>
2026-03-09 23:59:14 +0000

    Update address in Footer component

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -51,8 +51,7 @@ export default function Footer() {
             <Box sx={{ display: "flex", alignItems: "start", gap: 1 }}>
               <FiMapPin size={16} />
               <Typography variant="body2" color="grey.300">
-                Intersection du boulevard Anoual et du boulevard Abdelmoumen, Centre Miraj, 4ᵉ étage, porte 54, Casablanca, à côté de la maison Opel.
-              </Typography>
+45 rue de l’atlas 3è àme étage N°10 , Maarif , Casablanca              </Typography>
             </Box>
           </Stack>
         </Box>
